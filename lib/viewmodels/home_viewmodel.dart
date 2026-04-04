4ca012e93b7f23bbdc6e1a7caefc8b4be3976cdf
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-05 00:55:30 +0800

    Update home_viewmodel.dart

--- lib/viewmodels/home_viewmodel.dart
+++ lib/viewmodels/home_viewmodel.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
-import 'package:ios_tvbox/core/spider_manager.dart';
-import 'package:ios_tvbox/models/video_model.dart';
+import '../core/spider_manager.dart';
+import '../models/video_model.dart';
 
 class HomeViewModel extends ChangeNotifier {
   List<VideoModel> videoList = [];
