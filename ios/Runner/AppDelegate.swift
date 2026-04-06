fb8d1212a39077e06badab6c57628f494dd31f71
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-06 15:54:18 +0800

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller = window?.rootViewController as! FlutterViewController
+    // ✅ 必须显式注册自定义插件，因为自定义插件不会被自动注册
+    NodeJSBridge.register(with: controller.registrar(forPlugin: "NodeJSBridge")!)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
