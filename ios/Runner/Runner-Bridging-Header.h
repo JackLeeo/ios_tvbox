2739bed85afe4f2d58cdc9c6cd49f9701bb79af7
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-07 12:25:42 +0800

    Update Runner-Bridging-Header.h

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -4,6 +4,5 @@
 // Flutter自动生成的插件注册头文件，必须保留，不然所有第三方插件都用不了
 #import "GeneratedPluginRegistrant.h"
 // NodeMobile的头文件，我们加了搜索路径后，Xcode就能找到它了
-#import <NodeMobile/NodeMobile.h>
-
+#import "../Frameworks/NodeMobile.xcframework/ios-arm64/NodeMobile.framework/Headers/NodeMobile.h"
 #endif
