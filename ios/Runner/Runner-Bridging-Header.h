8029a7324294afb06d827ee03db14c15e1f63f3d
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-07 11:20:34 +0800

    Update Runner-Bridging-Header.h

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,3 +1,3 @@
 #import "GeneratedPluginRegistrant.h"
 // 新增：导入nodejs-mobile的头文件，让Swift可以访问这个Framework的API
-#import <NodeMobile/NodeMobile.h>
+#import "../Frameworks/NodeMobile.xcframework/ios-arm64/NodeMobile.framework/Headers/NodeMobile.h"
