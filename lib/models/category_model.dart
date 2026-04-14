619362b16d4536e8de77e776e4acebcaf7739db7
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-14 17:48:02 +0800

    Update category_model.dart

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,82 +1,19 @@
 class CategoryModel {
-  final String tid;
+  final String id;
   final String name;
-  final List<FilterModel>? filters;
+  final String type;
 
   CategoryModel({
-    required this.tid,
+    required this.id,
     required this.name,
-    this.filters,
+    required this.type,
   });
 
   factory CategoryModel.fromJson(Map<String, dynamic> json) {
     return CategoryModel(
-      tid: json['tid'] ?? '',
+      id: json['id'] ?? '',
       name: json['name'] ?? '',
-      filters: json['filters'] != null
-          ? (json['filters'] as List)
-              .map((e) => FilterModel.fromJson(e))
-              .toList()
-          : null,
+      type: json['type'] ?? '',
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'tid': tid,
-      'name': name,
-      'filters': filters?.map((e) => e.toJson()).toList(),
-    };
-  }
-}
-
-class FilterModel {
-  final String key;
-  final String name;
-  final List<FilterValue> values;
-
-  FilterModel({
-    required this.key,
-    required this.name,
-    required this.values,
-  });
-
-  factory FilterModel.fromJson(Map<String, dynamic> json) {
-    return FilterModel(
-      key: json['key'] ?? '',
-      name: json['name'] ?? '',
-      values: (json['values'] as List)
-          .map((e) => FilterValue.fromJson(e))
-          .toList(),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'key': key,
-      'name': name,
-      'values': values.map((e) => e.toJson()).toList(),
-    };
-  }
-}
-
-class FilterValue {
-  final String n;
-  final String v;
-
-  FilterValue({required this.n, required this.v});
-
-  factory FilterValue.fromJson(Map<String, dynamic> json) {
-    return FilterValue(
-      n: json['n'] ?? '',
-      v: json['v'] ?? '',
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'n': n,
-      'v': v,
-    };
   }
 }
