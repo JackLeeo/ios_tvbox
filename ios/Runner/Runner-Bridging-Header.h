c0aa68a2fab4299912b7ed9236e9798a105e320d
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-15 18:33:38 +0800

    Update Runner-Bridging-Header.h

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,8 +1,6 @@
 #ifndef Runner_Bridging_Header_h
 #define Runner_Bridging_Header_h
-
 #import "GeneratedPluginRegistrant.h"
 // 直接用相对路径，Xcode直接读文件，不绕规则
-#import "../Frameworks/NodeMobile.xcframework/ios-arm64/NodeMobile.framework/Headers/NodeMobile.h"
-
+#import "../Frameworks/NodeMobile.framework/Headers/NodeMobile.h"
 #endif
