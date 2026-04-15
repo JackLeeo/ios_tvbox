663788a7e11a7b394c5829be2c621163b1ff0b6d
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-15 22:17:23 +0800

    Update Runner-Bridging-Header.h

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,6 +1,8 @@
 #ifndef Runner_Bridging_Header_h
 #define Runner_Bridging_Header_h
 #import "GeneratedPluginRegistrant.h"
-// 用模块导入，自动适配xcframework的路径，不会有路径冲突
-@import NodeMobile;
+// 声明官方nodejs-mobile的C API函数
+int nodeStart(int argc, char *argv[]);
+void nodejs_channel_send(NSString *message);
+void nodejs_channel_set_listener(void (^listener)(NSString *message));
 #endif
