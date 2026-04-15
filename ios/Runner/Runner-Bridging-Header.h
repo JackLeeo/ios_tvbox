355c8524b04af1f121efdd6c7d91871d3e64f54a
JackLeeo <130451237+JackLeeo@users.noreply.github.com>
2026-04-15 23:25:14 +0800

    Update Runner-Bridging-Header.h

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,6 +1,6 @@
 #ifndef Runner_Bridging_Header_h
 #define Runner_Bridging_Header_h
 #import "GeneratedPluginRegistrant.h"
-// 官方nodejs-mobile唯一有的C函数，工作流里也验证过这个符号存在
+// 官方nodejs-mobile唯一支持的函数，工作流也验证过这个符号
 int node_start(int argc, char *argv[]);
 #endif
